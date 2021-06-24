14e9a95a80938ec3cb3eb06039c3c12f545ea11e
Andres David Betin <andres.betin@qrvey.com>
2021-06-24 08:16:03 -0500

    set up stenciljs project

---
+++ i18next-stenciljs-poc/stencil.config.ts
@@ -0,0 +1,21 @@
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'i18next-stenciljs-poc',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements-bundle',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+};
