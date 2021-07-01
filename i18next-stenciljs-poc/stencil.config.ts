b776b88a5c358745b3ca9d66c21dd96280fb7816
Andres David Betin <andres.betin@qrvey.com>
2021-06-30 22:02:09 -0500

    stenciljs config for copying files

--- i18next-stenciljs-poc/stencil.config.ts
+++ i18next-stenciljs-poc/stencil.config.ts
@@ -14,7 +14,7 @@ export const config: Config = {
       type: 'www',
       serviceWorker: null, // disable service workers
       copy: [
-        { src: '/src/*.json', dest: "build/i18n" },
+        { src: 'components/**/*.i18n.json', dest: "build/i18n" },
       ]
     },
   ],
